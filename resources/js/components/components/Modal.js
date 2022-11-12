a216d0ea045fa28ba1884629afc9c13fe2f9ada8
Marcin Kośka <m.koska2@wp.pl>
2022-11-12 11:12:34 +0100

    Before redux

--- resources/js/components/components/Modal.js
+++ resources/js/components/components/Modal.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 import { Container, CloseButton, FormContainer } from "./Modal.css";
-import AddBoard from "./AddBoard";
 
 const Modal = ({ data, setFormData }) => {
-    console.log(data);
+    // console.log(data);
 
     const action = data.action;
 
@@ -14,6 +13,7 @@ const Modal = ({ data, setFormData }) => {
                 <CloseButton onClick={() => setFormData(null)} size={24} />
                 <FormContainer>
                     {action == "addBoard" && <AddBoard />}
+                    {action == "editBoard" && <EditBoard data={data} />}
                 </FormContainer>
             </Container>
         </>
